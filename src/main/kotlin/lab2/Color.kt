3eb9374a477f2eb27276f840347cd21211a05b70
xdvad <xdvadiks@bk.ru>
2022-04-08 11:04:11 +0300

    interfaces & class color

--- src/main/kotlin/lab2/Color.kt
+++ src/main/kotlin/lab2/Color.kt
@@ -1,4 +1,23 @@
 package lab2
 
-class Color {
+// class for color information storage
+class Color(red: Int, green: Int, blue: Int, alpha: Int) {
+    private var red: Int
+    private val green: Int
+    private var blue: Int
+    private val alpha: Int
+
+    init {
+        if (red !in 0..255 || green !in 0..255 || blue !in 0..255 || alpha !in 0..100) {
+            throw IllegalArgumentException("Color there isn't in RGBA format")
+        }
+        this.red = red
+        this.green = green
+        this.blue = blue
+        this.alpha = alpha
+    }
+
+    override fun toString(): String {
+        return "(red=$red, green=$green, blue=$blue, alpha=$alpha)"
+    }
 }
--- src/main/kotlin/lab2/Interfaces.kt
+++ src/main/kotlin/lab2/Interfaces.kt
@@ -1,4 +1,12 @@
 package lab2
 
-interface Interfaces {
+// interface of 2D shapes
+interface Shape2d {
+    fun calculateArea(): Double // shape area calculation
+}
+
+// interface of colored 2D shapes
+interface ColoredShape2d : Shape2d {
+    val borderColor: Color
+    val fillColor: Color
 }
