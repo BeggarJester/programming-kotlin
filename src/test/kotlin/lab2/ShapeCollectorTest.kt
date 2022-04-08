6ebc78df6e1266ca0b9924ed9176d917b76ef5fe
BeggarJester <spiridonovalex007@mail.ru>
2022-04-08 11:22:52 +0300

    created ShapeCollectorTest file

---
+++ src/test/kotlin/lab2/ShapeCollectorTest.kt
@@ -0,0 +1,56 @@
+package lab2
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class ShapeCollectorTest {
+
+    @Test
+    fun `test add element to ShapeCollector`() {
+    }
+
+    @Test
+    fun `test return list of elements with minimum area`() {
+    }
+
+    @Test
+    fun `test return list of elements with maximum area`() {
+    }
+
+    @Test
+    fun `test return the sum of all elements areas`() {
+    }
+
+    @Test
+    fun `test return list of searched elements by user fill color`() {
+    }
+
+    @Test
+    fun `test return list of searched elements by user border color`() {
+    }
+
+    @Test
+    fun `test return the ShapeCollector`() {
+    }
+
+    @Test
+    fun `test return the ShapeCollector size`() {
+    }
+
+    @Test
+    fun `test return created map by elements fill color`() {
+    }
+
+    @Test
+    fun `test return created map by elements border color`() {
+    }
+
+    @Test
+    fun `test return list of filtered elements by user shape type`() {
+    }
+
+    @Test
+    fun `test return the ShapeCollector by string`() {
+    }
+}
