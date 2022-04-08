b802b5521505528138587c3124da88e2a3ecaf6a
xdvad <xdvadiks@bk.ru>
2022-04-08 11:29:29 +0300

    add some tests

--- src/test/kotlin/lab2/ShapeCollectorTest.kt
+++ src/test/kotlin/lab2/ShapeCollectorTest.kt
@@ -8,26 +8,167 @@ internal class ShapeCollectorTest {
 
     @Test
     fun `test add element to ShapeCollector`() {
+        val shape = Circle(
+            Color(255, 255, 255, 0),
+            Color(1, 1, 1, 0),
+            1.5
+        )
+        val myShapeCollector = ShapeCollector()
+        assertEquals(0, myShapeCollector.getListSize())
+        myShapeCollector.insertShape2d(shape)
+        assertEquals(1, myShapeCollector.getListSize())
+        assertEquals(
+            listOf(shape),
+            myShapeCollector.getList()
+        )
     }
 
     @Test
     fun `test return list of elements with minimum area`() {
+        val shape1 = Circle(
+            Color(255, 255, 255, 0),
+            Color(1, 1, 1, 0),
+            2.0
+        )
+        val shape2 = Square(
+            Color(255, 255, 255, 0),
+            Color(1, 1, 1, 0),
+            5.0
+        )
+        val myShapeCollector = ShapeCollector()
+        assertEquals(0, myShapeCollector.getListSize())
+        assertEquals(
+            true,
+            myShapeCollector.minArea().isEmpty()
+        )
+        myShapeCollector.insertShape2d(shape1)
+        myShapeCollector.insertShape2d(shape2)
+        assertEquals(2, myShapeCollector.getListSize())
+        assertEquals(
+            listOf(shape1),
+            myShapeCollector.minArea()
+        )
     }
 
     @Test
     fun `test return list of elements with maximum area`() {
+        val shape1 = Circle(
+            Color(255, 255, 255, 0),
+            Color(1, 1, 1, 0),
+            2.0
+        )
+        val shape2 = Square(
+            Color(255, 255, 255, 0),
+            Color(1, 1, 1, 0),
+            5.0
+        )
+        val myShapeCollector = ShapeCollector()
+        assertEquals(0, myShapeCollector.getListSize())
+        assertEquals(
+            true,
+            myShapeCollector.maxArea().isEmpty()
+        )
+        myShapeCollector.insertShape2d(shape1)
+        myShapeCollector.insertShape2d(shape2)
+        assertEquals(2, myShapeCollector.getListSize())
+        assertEquals(
+            listOf(shape2),
+            myShapeCollector.maxArea()
+        )
     }
 
     @Test
     fun `test return the sum of all elements areas`() {
+        val shape1 = Rectangle(
+            Color(255, 255, 255, 0),
+            Color(1, 1, 1, 0),
+            2.0,
+            5.0
+        )
+        val shape2 = Square(
+            Color(255, 255, 255, 0),
+            Color(1, 1, 1, 0),
+            5.0
+        )
+        val myShapeCollector = ShapeCollector()
+        assertEquals(
+            0,
+            myShapeCollector.getListSize()
+        )
+        assertEquals(
+            0.0,
+            myShapeCollector.sumAllAreas()
+        )
+        myShapeCollector.insertShape2d(shape1)
+        myShapeCollector.insertShape2d(shape2)
+        assertEquals(
+            2,
+            myShapeCollector.getListSize()
+        )
+        assertEquals(
+            35.0,
+            myShapeCollector.sumAllAreas()
+        )
     }
 
     @Test
     fun `test return list of searched elements by user fill color`() {
+        val colour1 = Color(255, 255, 255, 0)
+        val colour2 = Color(1, 1, 1, 0)
+        val shape1 = Rectangle(
+            colour1,
+            colour1,
+            2.0,
+            5.0
+        )
+        val shape2 = Square(
+            colour2,
+            colour2,
+            5.0
+        )
+        val myShapeCollector = ShapeCollector()
+        assertEquals(0, myShapeCollector.getListSize())
+        assertEquals(
+            true,
+            myShapeCollector.searchByFillColor(colour1).isEmpty()
+        )
+        myShapeCollector.insertShape2d(shape1)
+        myShapeCollector.insertShape2d(shape2)
+        assertEquals(2, myShapeCollector.getListSize())
+        assertEquals(
+            listOf(shape1),
+            myShapeCollector.searchByFillColor(colour1)
+        )
     }
 
     @Test
     fun `test return list of searched elements by user border color`() {
+        val colour1 = Color(255, 255, 255, 0)
+        val colour2 = Color(1, 1, 1, 0)
+        val shape1 = Rectangle(
+            colour1,
+            colour1,
+            2.0,
+            5.0
+        )
+        val shape2 = Square(
+            colour2,
+            colour2,
+            5.0
+        )
+        val myShapeCollector = ShapeCollector()
+        assertEquals(0, myShapeCollector.getListSize())
+        assertEquals(
+            true,
+            myShapeCollector.searchByBorderColor(colour1).isEmpty()
+        )
+        myShapeCollector.insertShape2d(shape1)
+        myShapeCollector.insertShape2d(shape2)
+        assertEquals(2, myShapeCollector.getListSize())
+        assertEquals(
+            listOf(shape2),
+            myShapeCollector.searchByBorderColor(colour2)
+        )
     }
 
     @Test
