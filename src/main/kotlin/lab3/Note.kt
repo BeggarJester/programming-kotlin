8efe09b5f80adcf088037062ff279a4559ed369a
BeggarJester <spiridonovalex007@mail.ru>
2022-04-16 21:52:28 +0300

    created empty files of lab3

---
+++ src/main/kotlin/lab3/Note.kt
@@ -0,0 +1,3 @@
+package lab3
+
+sealed class Note
--- src/main/kotlin/lab3/main.kt
+++ src/main/kotlin/lab3/main.kt
@@ -1,29 +1,6 @@
 package lab3
 
-interface NoteService: Note {
-    fun getAllNotes(): List<Note>
-    fun getAllTextNotes(): List<Note.TextNote>
-    fun getAllTasks(): List<Note.Task>
-    fun getAllLinks(): List<Note.Link>
-
-    fun createTextNote(title: String, content: String): Note.TextNote
-}
-
-interface Note{
-    val title: String
-    var date : java.time.LocalDate
-}
-
-// class Circle(override val borderColor: Color, override val fillColor: Color, radius: Double) : ColoredShape2d
-
-class TextNote(override val title: String, override var date : java.time.LocalDate, text: String): Note{
-    private val content: String
-    init{
-        content = text
-    }
-
-}
 
 fun main(){
-    println("hello")
+
 }
