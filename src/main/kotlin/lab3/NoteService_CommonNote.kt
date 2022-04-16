3f0b327d2eba6b6b61d408dc62e74aba83fe0ba5
xdvad <xdvadiks@bk.ru>
2022-04-16 21:55:29 +0300

    add interfaces

--- src/main/kotlin/lab3/NoteService_CommonNote.kt
+++ src/main/kotlin/lab3/NoteService_CommonNote.kt
@@ -1,4 +1,23 @@
 package lab3
 
-interface NoteService_CommonNote {
+import java.net.URL
+import java.time.LocalDate
+
+interface NoteService {
+    fun getAllNotes(): List<Note>
+    fun getAllTextNotes(): List<Note.TextNote>
+    fun getAllTasks(): List<Note.Task>
+    fun getAllLinks(): List<Note.Link>
+    fun createTextNote(title: String, content: String): Note.TextNote
+    fun createTaskNote(title: String, description: String, deadline: LocalDate): Note.Task
+    fun createLinkNote(title: String, url: URL): Note.Link
+    fun removeNote(title: String, typeName: String)
+    fun find(title: String, typeName: String): List<Note>
+    fun getSortedByTitle(): List<Note>
+    fun getSortedByDate(): List<Note>
+}
+
+interface CommonNote {
+    val title: String
+    val date: LocalDate
 }
