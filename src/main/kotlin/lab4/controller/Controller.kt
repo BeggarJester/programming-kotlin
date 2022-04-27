935cefd9aca48cdc763072629d91034e7424f9f1
xdvad <xdvadiks@bk.ru>
2022-04-27 19:56:57 +0300

    add controller class

--- src/main/kotlin/lab4/controller/Controller.kt
+++ src/main/kotlin/lab4/controller/Controller.kt
@@ -1,2 +1,21 @@
 package lab4.controller
 
+import lab4.model.Model
+import lab4.model.State
+
+class Controller(private val model: Model) {
+    init {
+        startGame()
+    }
+
+    private fun startGame() {
+        while (model.state == State.MOVE) {
+            val input = readln()
+            try {
+                model.doMove(input.lowercase())
+            } catch (e: Exception) {
+                println(e.message)
+            }
+        }
+    }
+}
