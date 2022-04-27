1a3bdcc0491167743206c01e722ebde03802740f
BeggarJester <spiridonovalex007@mail.ru>
2022-04-27 20:00:51 +0300

    add consoleMain

--- src/main/kotlin/lab4/consoleMain/main.kt
+++ src/main/kotlin/lab4/consoleMain/main.kt
@@ -1,2 +1,13 @@
 package lab4.consoleMain
 
+import lab4.controller.Controller
+import lab4.model.Model
+import lab4.viewer.ConsoleUi
+
+fun main() {
+
+    val maze = Model()
+    ConsoleUi(maze)
+    Controller(maze)
+
+}
