88722f507220bec820e55da79d23e3a2e838ece5
BeggarJester <spiridonovalex007@mail.ru>
2022-09-09 23:15:19 +0300

    hope

--- src/main/kotlin/coursework/main.kt
+++ src/main/kotlin/coursework/main.kt
@@ -4,7 +4,7 @@ import coursework.view.GameUI
 import javax.swing.SwingUtilities
 
 fun main() {
-
+    println("hi")
     SwingUtilities.invokeLater {
         val game = GameUI()
         game.isVisible = true
