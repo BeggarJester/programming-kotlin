2452eba27e0d5cfdb2ca899c12a94ab1b8531d55
BeggarJester <spiridonovalex007@mail.ru>
2022-09-09 23:20:36 +0300

    initial commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,4 @@
-import matrix.Matrix
 
 fun main() {
-    val A = Matrix(
-        arrayOf(
-            doubleArrayOf(1.0, 2.0),
-            doubleArrayOf(3.0, 4.0),
-        )
-    )
-
-    val element = A[0, 0]
-
-    A[0, 0] = 100.0
+   println("hello, raccoons!")
 }
--- src/main/kotlin/lab1/Book.kt
+++
@@ -1,4 +0,0 @@
-package lab1
-
-// data class Book for books information storage
-data class Book(val title: String, val authors: List<String>, val year: Int)
